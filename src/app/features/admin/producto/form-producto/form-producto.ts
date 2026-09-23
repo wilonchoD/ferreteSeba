580e0647286c0ddd8f4dee7b7bd1e23db8bfaf83
wilonchoD <dorschsebita@gmail.com>
2026-09-23 19:48:49 -0300

    creacion de producto funcional, mas avisos de eliminacion y creacion de productos

--- src/app/features/admin/producto/form-producto/form-producto.ts
+++ src/app/features/admin/producto/form-producto/form-producto.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, RequiredValidator } from '@angular/forms';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
@@ -9,6 +9,9 @@ import { TextareaModule } from 'primeng/textarea';
 import { ProductosService } from '../producto.service';
 import { CategoriasService } from '../../categorias/categorias.service';
 import { MarcasService } from '../../marca/marca.service';
+import { Categoria } from '../../../../core/models/categoria.model';
+import { Marcas } from '../../../../core/models/marca.model';
+import { Producto } from '../../../../core/models/producto.model';
 import { SelectModule } from 'primeng/select';
 import { MessageService } from 'primeng/api';
 
@@ -36,11 +39,15 @@ export class FormProducto {
   private route = inject(ActivatedRoute);
   private router = inject(Router);
   private productosService = inject(ProductosService);
-  private categoriasService = inject(CategoriasService)
-  private marcasService = inject(MarcasService)
+  private categoriasService = inject(CategoriasService);
+  private marcasService = inject(MarcasService);
+  private toastService = inject(MessageService)
 
-  isEditMode = false;
-  productoId: string | null = null;
+
+
+  protected categorias = signal<Categoria[]>([]);
+  protected marcas = signal<Marcas[]>([]);
+
 
   showDebug: boolean = true;
 
@@ -54,13 +61,13 @@ export class FormProducto {
       Validators.required
     ]],
 
-    modelo: ['',[
+    modelo: ['', [
       Validators.required,
       Validators.minLength(2),
       Validators.maxLength(15)
     ]],
 
-    color: ['',[
+    color: ['', [
       Validators.required,
       Validators.minLength(2),
       Validators.maxLength(10)
@@ -89,7 +96,7 @@ export class FormProducto {
 
     url_imagen: [''],
 
-    estado: ['', [
+    estado: [null as 'Activo' | 'Inactivo' | null, [
       Validators.required
     ]],
 
@@ -97,29 +104,67 @@ export class FormProducto {
 
   });
 
+  ngOnInit() {
+    this.cargarCategorias();
+    this.cargarMarcas();
 
-  guardarProducto() {
-    if (!this.formProducto.invalid) {
-      const producto: any = this.formProducto.value;
-
-      this.productosService.guardarProducto(producto).subscribe(
-        (data) => {
-          alert('producto guardado con éxito');
-          this.formProducto.reset();
-
-
-        }
-      )
+    if (this.productosService.productoEditar() != null) {
+      this.formProducto.patchValue({
+        nombre: this.productosService.productoEditar()?.nombre,
+        codigo: this.productosService.productoEditar()?.codigo,
+        modelo: this.productosService.productoEditar()?.modelo,
+        color: this.productosService.productoEditar()?.color,
+        descripcion: this.productosService.productoEditar()?.descripcion,
+        precio: this.productosService.productoEditar()?.precio,
+        stock: this.productosService.productoEditar()?.stock,
+        categoria_id: this.productosService.productoEditar()?.categoria_id,
+        marca_id: this.productosService.productoEditar()?.marca_id,
+        url_imagen: this.productosService.productoEditar()?.url_imagen,
+        estado: this.productosService.productoEditar()?.estado,
+      });
+    } else {
+      this.formProducto.reset();
     }
   }
 
-  categorias = []
-  marcas = []
+  guardarProducto() {
+    if (!this.formProducto.invalid) {
+      const producto: Producto = {
+        nombre: this.formProducto.value.nombre!,
+        codigo: this.formProducto.value.codigo!,
+        modelo: this.formProducto.value.modelo!,
+        color: this.formProducto.value.color!,
+        descripcion: this.formProducto.value.descripcion!,
+        precio: this.formProducto.value.precio!,
+        stock: this.formProducto.value.stock!,
+        categoria_id: this.formProducto.value.categoria_id!,
+        marca_id: this.formProducto.value.marca_id!,
+        url_imagen: this.formProducto.value.url_imagen!,
+        estado: this.formProducto.value.estado!,
+      };
+      if (this.productosService.productoEditar() == null) {
+        this.productosService.guardarProducto(producto).subscribe(
+          (data) => {
+            this.toastService.add({ severity: 'success', summary: data.message });
+            this.finalizarGuardado();
+          }
+        );
+      } else {
+        this.productosService.actualizarProducto(producto, this.productosService.productoEditar()?.id!).subscribe(
+          (data) => {
+            this.toastService.add({ severity: 'success', summary: data.message });
+            this.finalizarGuardado();
+          }
+        );
+      }
+    }
+  }
+
 
   cargarCategorias() {
     this.categoriasService.obtenerCategorias().subscribe(
       (categorias) => {
-        this.categorias = categorias;
+        this.categorias.set(categorias);
       }
     )
   }
@@ -127,13 +172,13 @@ export class FormProducto {
   cargarMarcas() {
     this.marcasService.obtenerMarcas().subscribe(
       (marcas) => {
-        this.marcas = marcas;
+        this.marcas.set(marcas);
       }
     )
   }
-  ngOnInit(): void {
-    this.cargarCategorias();
-    this.cargarMarcas();
-  }
 
+  finalizarGuardado() {
+    this.formProducto.reset();
+    this.router.navigate(['/admin/lista-producto']);
+  }
 }
